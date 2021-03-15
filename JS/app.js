91328e042d733ccfe19d3ba9dad7e205e4732433
Abdallah Abu Khurma <abdallahabukhurma@gmail.com>
2021-03-15 19:00:45 +0200

    work done

---
+++ JS/app.js
@@ -0,0 +1,125 @@
+'use strict';
+
+let welcome = alert ('Welcome to About Me page');
+
+let name = prompt ('Please enter your name');
+
+let doing = prompt ('Am I currently doing something I love?');
+
+if(doing.toLowerCase() === 'yes')
+{
+    alert ('the answer is correct');
+}
+else
+{
+    alert ('the answer is incorrect');
+}
+
+let challenge = prompt ('Do I see my challenges as a disadvantage?');
+
+if(challenge.toLowerCase() === 'yes')
+{
+    alert ('The answer is incorrect');
+}
+else
+{
+    alert ('The answer is correct');
+}
+
+let personal = prompt ('Am I a good person?');
+
+if(personal.toLowerCase() === 'yes')
+{
+    alert ('the answer is correct');
+}
+else
+{
+    alert ('the answer is incorrect');
+}
+
+let sleep = prompt ('Have I ever fallen asleep at school or work?');
+
+if (sleep.toLowerCase() === 'yes')
+{
+    alert ('the answer is incorrect');
+}
+else
+{
+    alert ('the answer is correct');
+}
+
+let school = prompt ('Have I ever run away from my school?');
+
+if (school.toLowerCase() === 'yes')
+{
+    alert ('the answer is correct');
+}
+else
+{
+    alert ('the answer is incorrect, actually I did it twice or more ^.^');
+}
+
+switch(doing)
+{
+    case 'yes':
+        console.log('yes');
+        break;
+    case 'No':
+        console.log('No');
+        break;
+    default:
+        console.log(doing);
+
+}
+
+switch(challenge)
+{
+    case 'yes':
+        console.log('yes');
+        break;
+    case 'No':
+        console.log('No');
+        break;
+    default:
+        console.log(challenge);
+
+}
+
+switch(personal)
+{
+    case 'yes':
+        console.log('yes');
+        break;
+    case 'No':
+        console.log('No');
+        break;
+    default:
+        console.log(personal);
+
+}
+
+switch(sleep)
+{
+    case 'yes':
+        console.log('yes');
+        break;
+    case 'No':
+        console.log('No');
+        break;
+    default:
+        console.log(sleep);
+
+}
+
+switch(school)
+{
+    case 'yes':
+        console.log('yes');
+        break;
+    case 'No':
+        console.log('No');
+        break;
+    default:
+        console.log(school);
+
+}
